450b0dad33cd4d4c5796570ef2596dac5c098651
User <renan.uchoa.m@gmail.com>
2022-05-22 17:38:26 -0300

    Create SellerDaoJDBC, DaoFactory and findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
-import model.entities.Department;
+import model.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 import java.util.Date;
@@ -8,9 +9,9 @@ import java.util.Date;
 public class Program {
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        Seller seller = new Seller(21, "Bob", "bob@email.com", new Date(), 3000.00, obj);
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
